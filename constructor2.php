36e39c79e580300c8e28dda80a06a30bf6d89ca4
mulugeta-89 <mulugetahail89@gmail.com>
2023-04-11 20:19:47 +0300

    constructor promoted properties

---
+++ constructor2.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Product {
+    public function __construct(public $name = "pasta", public $price = 30) //writing this line of 
+    //helps to not write the below lines of code, bro
+    {
+        // $this->name = $name;
+        // $this->price = $price;
+    }
+    public function get_all(){
+        print $this->name . " ". "->" . " " . $this->price;
+    }
+}
+$product = new Product(price:90);
+$product->get_all();
+
+
+
+
+?>
