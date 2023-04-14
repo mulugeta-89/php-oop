2cacb69e2609614512e68b6fd67a54301c063195
mulugeta-89 <mulugetahail89@gmail.com>
2023-04-14 21:12:55 +0300

    Revert "typed-properties"
    This reverts commit 5c87aeb98fdba512d6e688cd1bd052ff8fcddc16.

--- typed_properties/caller.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "song.php";
-$song = new Song("alemtam", "shimels bekele");
-$song->title = ["abebe", "kebede", "shimels"];
-var_dump($song->title);
-print $song->title . PHP_EOL;
-print $song->author . PHP_EOL;
