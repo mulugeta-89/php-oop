5c87aeb98fdba512d6e688cd1bd052ff8fcddc16
mulugeta-89 <mulugetahail89@gmail.com>
2023-04-14 21:10:05 +0300

    typed-properties

---
+++ typed_properties/song.php
@@ -0,0 +1,11 @@
+<?php
+
+class Song {
+    public string $title; //specifying the type of the property help us to limit the ability of assigning value to it
+    public string $author;
+    function __construct(string $title, string $author){
+        $this->title = $title;
+        $this->author = $author;
+    }
+
+}
